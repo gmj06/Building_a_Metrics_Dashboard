006d1004678a69c4fbdc7f508baa3a3d4e9dcfdb
Bhoopalam <chandrub@awl65333l.lan>
2021-11-11 20:51:39 -0800

    Added tracing to backend and  ServiceMonitor to all apps

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -1,11 +1,52 @@
 from flask import Flask, render_template, request
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app, group_by='endpoint')
+metrics.info("app_info", "App Info", version="1.0.3")
+endpoint_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
+
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer('frontend')
+tracing= FlaskTracing(tracer, True, app)
 
 @app.route('/')
+@endpoint_counter
 def homepage():
+    with tracer.start_span('frontend home') as span:
+        span.set_tag('first-tag', "Hello World - frontend")
     return render_template("main.html")
 
+
+# register additional default metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
 if __name__ == "__main__":
     app.run()
--- reference-app/trial/app.py
+++ reference-app/trial/app.py
@@ -1,36 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 
 from jaeger_client import Config
-from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
-from opentelemetry import trace
-from opentelemetry.exporter import jaeger
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    ConsoleSpanExporter,
-    SimpleExportSpanProcessor,
-)
-
-trace.set_tracer_provider(TracerProvider())
-trace.get_tracer_provider().add_span_processor(
-    SimpleExportSpanProcessor(ConsoleSpanExporter())
-)
+from flask_opentracing import FlaskTracing
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
-
-
-#config = Config(
-#        config={},
-#        service_name='your-app-name',
-#        validate=True,
-#        metrics_factory=PrometheusMetricsFactory(service_name_label='your-app-name')
-#)
-#tracer = config.initialize_tracer()
+metrics = PrometheusMetrics(app, group_by='endpoint')
+metrics.info("app_info", "App Info", version="1.0.3")
+endpoint_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
 
 def init_tracer(service):
     logging.getLogger('').handlers = []
@@ -50,24 +30,24 @@ def init_tracer(service):
     # this call also sets opentracing.tracer
     return config.initialize_tracer()
 
-tracer = init_tracer('first-service')
+tracer = init_tracer('trial')
+tracing= FlaskTracing(tracer, True, app)
 
 @app.route('/')
+@endpoint_counter
 def homepage():
+    with tracer.start_span('hello world') as span:
+        span.set_tag('first-tag', "Hello World -Trial")
     return render_template("main.html")
-    with tracer.start_span('get-python-jobs') as span:
-        homepages = []
-        res = requests.get('https://jobs.github.com/positions.json?description=python')
-        span.set_tag('first-tag', len(res.json()))
-        for result in res.json():
-            try:
-                homepages.append(requests.get(result['company_url']))
-            except:
-                return "Unable to get site for %s" % result['company']
         
 
-
-    return jsonify(homepages)
+# register additional default metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
 if __name__ == "__main__":
-    app.run(debug=True,)
+    app.run()
